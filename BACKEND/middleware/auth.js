0d2215cf06956fe43c2671db228f1b65c6f5b1a9
Daniel Heilbron <134653973+st10082120@users.noreply.github.com>
2023-10-10 15:24:18 +0200

    Update auth.js

--- BACKEND/middleware/auth.js
+++ BACKEND/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-function auth (req, res, next) {
-    const token = req.header('x-auth-token');
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');//auth header
     let id;
 
     try {
